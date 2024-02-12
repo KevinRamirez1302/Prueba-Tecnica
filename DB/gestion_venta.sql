6b3ee242c97405ca5c9edffbf5f6f4df63b8ae81
Kevin <kevin.alexanderra1302@gmail.com>
2024-02-11 23:38:28 -0400

    fix DB

--- DB/gestion_venta.sql
+++ DB/gestion_venta.sql
@@ -30,7 +30,8 @@ CREATE TABLE usuario (
 
 INSERT INTO usuario (nombre, correo, usuario, clave) VALUES
 ( 'kevin ramirez', 'kevin.test@gmail.com', 'vendedor1', 'test1'),
-( 'kanye west', 'kanyeWest@gmail.com', 'vendedor2', 'test2');
+( 'kanye west', 'kanyeWest@gmail.com', 'vendedor2', 'test2'),
+( 'carlos galindez', 'carlosgalindez@gmail.com', 'vendedor3', 'vendedor3');
 
 
 --
@@ -43,6 +44,7 @@ CREATE TABLE cliente (
   telefono varchar(15) NOT NULL,
   direccion varchar(200) NOT NULL,
   usuario_id int(11) NOT NULL,
+  documento INT(10) NOT NULL,
   FOREIGN KEY(usuario_id) REFERENCES usuario(idusuario),
   PRIMARY KEY (idcliente)
   
@@ -53,8 +55,11 @@ CREATE TABLE cliente (
 
 
 INSERT INTO cliente (idcliente, nombre, telefono, direccion, usuario_id) VALUES
-(1, 'carlos gonzales', '8296826298', 'Centro', 1),
-(2, 'samuel flores', '2147483647', 'Carrera 28 entre 9 y 10', 2);
+('carlos gonzales', '8296826298', 'Centro', 1),
+('samuel flores', '2147483647', 'Carrera 28 entre 9 y 10', 2),
+('fernando galindez', '2147483647', 'Carrera 48 entre 9 y 10', 2),
+('leonel messi', '2147483647', 'Rosario,argentina', 2),
+('franco mazza', '2147483647', 'Pedregal', 1);
 
 --
 -- Se crea la tabla para productos
@@ -80,7 +85,10 @@ CREATE TABLE producto (
 INSERT INTO producto (imagen, descripcion, precio, categoria ,existencia, usuario_id) VALUES
 ( 'https://ecuador.clorox.com/wp-content/uploads/sites/6/2019/05/Botella-de-1000ml-original-960x500-1.png', ' Cloro', 1.99, 'limpieza',180, 1),
 ( 'https://costazul.sigo.com.ve/images/thumbs/0010613_limpia-vidrios-y-espejos-cristal-glax-brite-1-l_450.jpeg', 'Limpia Vidrios', 4.8,'limpia_vidrios', 50, 1),
-( 'https://www.laranitadelapaz.com.mx/images/thumbs/0005808_detergente-en-polvo-ariel-10850g_360.jpeg', 'Ariel Detergente en polvo', 5.8,'jabon_polvo', 50, 2);
+( 'https://www.laranitadelapaz.com.mx/images/thumbs/0005808_detergente-en-polvo-ariel-10850g_360.jpeg', 'Ariel Detergente en polvo', 6,'jabon_polvo', 50, 2),
+( 'https://www2.glade.com/~/media/glade/glade-mexico-frozen-website-content-update/sku/morning_freshness_glade_aerosol_400ml.jpg?la=es-cr', 'Glade', 5.8,'limpieza', 20, 2),
+( 'https://officenet.net.ve/61504-large_default/glade-aerosol-5-en-1-lavanda-360cm3.jpg', 'Glade Lavanda', 5.8,'limpieza', 50, 2),
+( 'https://s3.amazonaws.com/businessinsider.mx/wp-content/uploads/2020/10/22193541/business-insider-esponja-lavar-231020.jpg', 'Esponja', 1.8,'limpieza', 30, 1);
 
 
 -- Creamos la tabla para detalles de las ventas.
@@ -103,9 +111,12 @@ CREATE TABLE detalle_venta (
 --
 
 INSERT INTO detalle_venta (id_usuario,id_producto, cantidad, precio) VALUES
-( 2,1, 1,2),
-( 2,2, 1,2),
-( 1,3, 2,1);
+( 2,1, 1,20),
+( 2,2, 1,21),
+( 1,3, 2,10),
+( 1,4, 2,9),
+( 2,3, 2,7),
+( 1,3, 2,11);
 
 
 --
@@ -131,7 +142,11 @@ CREATE TABLE ventas (
 
 INSERT INTO ventas ( id_cliente, total, id_usuario, fecha) VALUES
 ( 1, '42.00', 1, '2021-05-16 14:35:54'),
-( 1, '39.00', 1, '2021-05-16 14:39:39');
+( 1, '39.00', 1, '2021-05-16 14:39:39'),
+( 2, '20.00', 2, '2021-05-16 14:39:39'),
+( 1, '19.00', 2, '2021-05-16 14:39:39'),
+( 2, '26.00', 2, '2021-05-16 14:39:39'),
+( 1, '32.00', 2, '2021-05-16 14:39:39');
 
 
 --
